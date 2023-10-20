3721091e8321b15b6b76dc4c43483885544405d3
wagtail37 <sumida.k.ad@m.titech.ac.jp>
2023-10-20 13:29:52 +0900

    first

--- streamlitDemo.py
+++ streamlitDemo.py
@@ -2,16 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-text = st.text_input(label='Message', value='Hello World!')
+text = st.text_input(label='Message', value='二酸化炭素センサwebアプリデモ')
 
-text2 = st.text_input(label='Message2', value=' こんにちは　世界')
-
-text3 = st.text_input(label='Message3', value='Hello 世界!')
 
 st.sidebar.header('サイドバー')
 st.sidebar.write(text)
-st.sidebar.write(text2)
-st.sidebar.write(text3)
+
 
 
 def main():
